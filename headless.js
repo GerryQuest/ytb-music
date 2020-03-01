ca502c317fa87480a43d18fcdaadd466918ee499
gerryquest <gerald.kessie@debenhams.com>
2020-03-01 20:50:12 +0000

    Update conditions

--- headless.js
+++ headless.js
@@ -7,7 +7,8 @@ const request_client = require("request-promise-native");
 var stopRequest = function (type, url) {
     if (type === "image" || type === "stylesheet" || type === "font")
         return true;
-    if (url.indexOf("data:") > -1)
+
+    if (url.startsWith("data:"))
         return true;
 };
 
@@ -33,48 +34,60 @@ exports.startDownload = async (ytbURL) => {
         let closeBrowser = async () => await browser.close();
         let requestFlag = false;
 
-        page.on("request", request => {
-            console.log("FIRING REQUEST");
-            console.log(request.url());
-
-            // Abort any requests that are: images, stylesheets or fonts
-            if (stopRequest(request.resourceType(), request.url()))
-                request.abort();
-
-            request_client({
-                uri: request.url(),
-                resolveWithFullResponse: true
-            }).then(response => {
-                let resHeader = response.headers || "";
-                let reqType = request.resourceType() || "";
-
-               
+        
+            page.on("request", request => {
                 if (!requestFlag)   {
+                    console.log("FIRING REQUEST");
+                    console.log(request.url());
 
-                    if (resHeader["content-type"] && resHeader["content-type"].indexOf("audio/webm") > -1) {
-                        requestFlag = true;
-                        console.log("Header::", resHeader["content-type"]);
-                        console.log("Requests URL::", request.url());
+                    
+                    // Abort any requests that are: images, stylesheets or fonts
+                    if (stopRequest(request.resourceType(), request.url()))
                         request.abort();
+                        
 
-                   // } else if (reqType === "image" || reqType === "stylesheet" || reqType === "font") {
-                        // request.abort();
-                    } else {
-                        request.continue().catch((error) => {console.log("Continue Error:", error)});
-                    }
-    
-                    result.push({resHeader, reqType});
-                } 
+                    request_client({
+                        uri: request.url(),
+                        resolveWithFullResponse: true
+                    }).then(response => {
+                        let resHeader = response.headers || "";
+                        let reqType = request.resourceType() || "";
+
+                    
+                        
+
+                            if (resHeader["content-type"] && 
+                                (resHeader["content-type"].indexOf("audio/webm") > -1) && request.url().indexOf("r6---") > -1) {
+                                requestFlag = true;
+                                console.log("Header::", resHeader["content-type"]);
+                                console.log("Requests URL::", response.url());
+                                request.abort();
+                                closeBrowser();
+
+                        // } else if (reqType === "image" || reqType === "stylesheet" || reqType === "font") {
+                                // request.abort();
+                            } else {
+                                request.continue().catch((error) => {console.log("Continue Error:", error)});
+                            }
+            
+                            result.push({resHeader, reqType});
+                        
 
 
-            }).catch(error => {
-                console.log(error);
-                request.abort().catch((error) => {console.log("Request Error:", error)});
-            });
-        });
+                    }).catch(error => {
+                        console.log(error);
+                        request.abort().catch((error) => {console.log("Request Error:", error)});
+                    });
 
-        console.log(ytbURL);
-        await page.goto(ytbURL, {waitUntil: "networkidle0"});
+                }
+             });
+
+             console.log(ytbURL);
+            await page.goto(ytbURL, {waitUntil: "networkidle0"});
+
+        
+
+        
         await browser.close();
 
 
